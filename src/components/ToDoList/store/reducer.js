412450491aade449b48136e965276ecff37ef164
STEIN <doruchenko.m.i@gmail.com>
2021-01-15 10:55:58 +0200

    fix

--- src/components/ToDoList/store/reducer.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from "redux";
-import {todolistTeduser} from "./todo/reducers";
-
-// COMBINE REDUCERS
-export let reducer = combineReducers({
-    toDosList: todolistTeduser,
-})
